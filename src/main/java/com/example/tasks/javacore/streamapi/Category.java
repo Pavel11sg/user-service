683384d15d28f4b164c2d614e3725ec5c5820bec
Pavel Shahoika <pavel11sg@gmail.com>
2025-07-07 21:07:03 +0300

    userservice: separate user-service

--- src/main/java/com/example/tasks/javacore/streamapi/Category.java
+++
@@ -1,8 +0,0 @@
-package com.example.tasks.javacore.streamapi;
-
-import lombok.Getter;
-
-@Getter
-public enum Category {
-	ELECTRONICS, CLOTHING, BOOKS, HOME, BEAUTY, TOYS
-}
--- src/main/java/com/example/tasks/javacore/streamapi/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.tasks.javacore.streamapi;
-
-import lombok.Getter;
-
-@Getter
-public enum OrderStatus {
-	NEW, PROCESSING, SHIPPED, DELIVERED, CANCELLED
-}
